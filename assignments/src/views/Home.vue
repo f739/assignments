6eb78d7260b04f8e8452b14470e52847911f6f7a
shimki <shimkifux@gmail.com>
2020-07-13 13:26:22 +0300

    first commit for assignments

---
+++ assignments/src/views/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-1">
+          <div>
+            <h4>הגדרות</h4>
+          </div>
+          <div>
+            <b-button class="col-lg-12">פרטי</b-button>
+          </div>
+          <div>
+            <b-button class="col-lg-12">ציבורי</b-button>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <h1 class="col-lg-12">כאן ניתן לכתוב את כל ההודעות</h1>
+          <b-table striped hover :items="items"></b-table>
+          <b-input class="col-lg-12" placeholder="הוסף הודעה" />
+        </div>
+        <div class="col-lg-3 red">
+          <h4 class="col-lg-12">:נושאים</h4>
+          <b-button class="col-lg-12 btn btn-default">HTML</b-button>
+          <b-button class="col-lg-12 btn btn-default">js</b-button>
+          <b-button class="col-lg-12 btn btn-default">css</b-button>
+          <b-button class="col-lg-12 btn btn-default">node</b-button>
+          <b-button class="col-lg-12 btn btn-default">mongoose</b-button>
+          <b-button class="col-lg-12 btn btn-default">express</b-button>
+          <b-button class="col-lg-12 btn btn-default">vue.js</b-button>
+          <b-button class="btn btn-default col-lg-6 b">הוסף נושא</b-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.container-fluid {
+  margin-top: 30px;
+}
+.btn-secondary {
+  margin-bottom: 15px;
+  background-color: #8299ae;
+}
+</style>
---
+++ assignments/src/views/login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="form">
+      <form action="http://localhot:3000/signup" v-show="showLogin === false">
+        <h1>הרשמה לפורום</h1>
+        <div>
+          <input type="text" name="name" id="name" />
+          <label for="name">:שם משתמש</label>
+        </div>
+        <div>
+          <input type="email" name="email" id="email" />
+          <label for="email">:דואר אלקטרוני</label>
+        </div>
+        <div>
+          <input type="password" name="paasword" id="password" />
+          <label for="password">:סיסמה</label>
+        </div>
+        <button>הירשם</button>
+        <div>
+          <b-button type="button" @click="showlogin">sign in</b-button>
+        </div>
+      </form>
+      <div v-show="showLogin">
+        <form action="http://localhot:3000/signin">
+          <h1>כניסה למשתמשים קיימים</h1>
+          <div>
+            <input type="email" name="email" id="email-in" />
+            <label for="email">:דואר אלקטרוני</label>
+          </div>
+          <div>
+            <input type="password" name="paasword" id="password-in" />
+            <label for="password">:סיסמה</label>
+          </div>
+          <button>היכנס</button>
+          <div>
+            <b-button type="button" @click="showlogin">sign up</b-button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showLogin: true
+    };
+  },
+  methods: {
+    showlogin() {
+      if (this.showLogin) {
+        this.showLogin = false;
+      } else if (!this.showLogin) {
+        this.showLogin = true;
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+html {
+  text-align: center;
+}
+form {
+  border: 2px solid #8299ae;
+  width: 450px;
+  height: 300px;
+  padding: 10px;
+  margin: 10px;
+  display: inline-block;
+}
+</style>
